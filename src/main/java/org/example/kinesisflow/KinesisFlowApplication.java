57ad8af2a1b7692e52ce0a669c8e9afa26ff12d4
Ouail <ouailbouazza6@gmail.com>
2025-06-30 18:03:14 +0200

    first commit

---
+++ src/main/java/org/example/kinesisflow/KinesisFlowApplication.java
@@ -0,0 +1,13 @@
+package org.example.kinesisflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinesisFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinesisFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kinesisflow/KinesisFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kinesisflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinesisFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
