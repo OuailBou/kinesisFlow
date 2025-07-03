57976b0ad04641a62f47150e9ba85789e1ce7bfb
Ouail <ouailbouazza6@gmail.com>
2025-07-03 16:38:32 +0200

    implemented jwt authenticaion with spring security

---
+++ src/main/java/org/example/kinesisflow/controller/UserController.java
@@ -0,0 +1,53 @@
+package org.example.kinesisflow.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.kinesisflow.model.AuthRequest;
+import org.example.kinesisflow.model.UserInfo;
+import org.example.kinesisflow.service.JwtService;
+import org.example.kinesisflow.service.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class UserController {
+
+    @Autowired
+    private UserInfoService service;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome this endpoint is not secure";
+    }
+
+    @PostMapping("/addNewUser")
+    public String addNewUser(@RequestBody UserInfo userInfo) {
+        return this.service.addUser(userInfo);
+    }
+
+    // Removed the role checks here as they are already managed in SecurityConfig
+
+    @PostMapping("/generateToken")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+        );
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getUsername());
+        } else {
+            throw new UsernameNotFoundException("Invalid user request!");
+        }
+    }
+}
